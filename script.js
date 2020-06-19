b174c834afea6ed37e94999568806752dd5c9a6f
mr-cade <cadelpetersen@gmail.com>
2020-06-19 11:55:19 -0600

    color coding based on time

--- script.js
+++ script.js
@@ -4,20 +4,45 @@ $("#currentDay").text(today);
 
 for (var i = 8; i <= 18; i++) {
     var timeBlock = document.createElement("div");
+    // formats based on # of digits
     if(i < 10) {
-        $(timeBlock).text("0"+i);
+        $(timeBlock).text("0" + i + ":00");
     } else {
-        $(timeBlock).text(i);
+        $(timeBlock).text(i + ":00");
     }
-    $(timeBlock).append("<textarea>")
+    // sets id and classes for css 
     $(timeBlock).attr({
         "id": i,
         "class": "row time-block hour"
     });
-    $(".container").append(timeBlock);
-    
+
     // conditional to assign past/present/future formatting
+    var currentHour = parseInt(moment().startOf('hour').format('h'));
+    console.log(currentHour);
+    console.log(timeBlock.id);
+    if (currentHour < timeBlock.id) {
+        $(timeBlock).addClass("future")
+    } else if (currentHour > timeBlock.id) {
+        $(timeBlock).addClass("past")
+    } else {
+        $(timeBlock).addClass("present")
+    }
+
+    // adds text input box to each time block
+    $(timeBlock).append("<textarea>")
+    
+
+    $(".container").append(timeBlock);
 
 }
 
+// make save button
+var saveBtn = document.createElement("button")
+$(saveBtn).text("Save");
+$(saveBtn).attr("class", "saveBtn")
+$(".container").append(saveBtn);
 
+// save data to local storage function
+function saveToLocal() {
+
+}

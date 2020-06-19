6a16ed160f86e2074672fe9792959ad73a1fece6
mr-cade <cadelpetersen@gmail.com>
2020-06-19 13:27:10 -0600

    events pushed to local storage

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // set today display
 var today = moment().format('MMM DD, YYYY');
+var eventInput = $('textarea');
+var localArr = [];
+
 $("#currentDay").text(today);
 
 for (var i = 8; i <= 18; i++) {
@@ -17,9 +20,8 @@ for (var i = 8; i <= 18; i++) {
     });
 
     // conditional to assign past/present/future formatting
-    var currentHour = parseInt(moment().startOf('hour').format('h'));
-    console.log(currentHour);
-    console.log(timeBlock.id);
+    var currentHour = parseInt(moment().startOf('hour').format('kk'));
+
     if (currentHour < timeBlock.id) {
         $(timeBlock).addClass("future")
     } else if (currentHour > timeBlock.id) {
@@ -30,19 +32,33 @@ for (var i = 8; i <= 18; i++) {
 
     // adds text input box to each time block
     $(timeBlock).append("<textarea>")
-    
-
+    // adds entire time block to the container
     $(".container").append(timeBlock);
-
+    // create save button
+    var saveBtn = document.createElement("button")
+    $(saveBtn).text("Save");
+    $(saveBtn).attr({
+        "id": i,
+        "class": "saveBtn"
+    })
+    $(timeBlock).append(saveBtn);
 }
+console.log("current hour: " + currentHour);
 
-// make save button
-var saveBtn = document.createElement("button")
-$(saveBtn).text("Save");
-$(saveBtn).attr("class", "saveBtn")
-$(".container").append(saveBtn);
+// get previously saved local events
+
 
 // save data to local storage function
-function saveToLocal() {
+function setLocal() {
+    console.log(this.id);
+    console.log(this.parentElement.children[0].value);
 
+    var timeAndText = {
+        time : event.target.id,
+        text : this.parentElement.children[0].value
+    }
+    localArr.push(timeAndText);
+    localStorage.setItem("events", JSON.stringify(localArr))
 }
+
+$(".saveBtn").on("click", setLocal);
